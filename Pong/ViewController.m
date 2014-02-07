5e42143709b37f8d7fa19f3a67fdb7bb8261e179
Metamorpheu <metamorpheu@gmail.com>
2014-02-07 20:51:28 +0000

    Commit 1

--- Pong/ViewController.m
+++ Pong/ViewController.m
@@ -16,8 +16,22 @@
 
 - (void)viewDidLoad
 {
+    
+    AnimatedBackground.animationImages = [NSArray arrayWithObjects:
+                                          [UIImage imageNamed:@"Menu01.png"],
+                                          [UIImage imageNamed:@"Menu02.png"],
+                                          [UIImage imageNamed:@"Menu03.png"],
+                                          [UIImage imageNamed:@"Menu04.png"],
+                                          [UIImage imageNamed:@"Menu05.png"],nil];
+    
+    [AnimatedBackground setAnimationRepeatCount:0];
+    AnimatedBackground.animationDuration = 5;
+    [AnimatedBackground startAnimating];
+    
+    
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
 }
 
 - (void)didReceiveMemoryWarning
