2943ddee961fdac67ad7eef3c8241fd29dd92e04
Metamorpheu <metamorpheu@gmail.com>
2014-02-07 23:12:18 +0000

    Commit test

---
+++ Pong/Test.m
@@ -0,0 +1,38 @@
+//
+//  Pong - Test.m
+//  Copyright 2014 Carlos Santos. All rights reserved.
+//
+//  Created by: Carlos Santos
+//
+
+    // Class under test
+#import "Game.h"
+
+    // Collaborators
+
+    // Test support
+#import <SenTestingKit/SenTestingKit.h>
+
+// Uncomment the next two lines to use OCHamcrest for test assertions:
+//#define HC_SHORTHAND
+//#import <OCHamcrestIOS/OCHamcrestIOS.h>
+
+// Uncomment the next two lines to use OCMockito for mock objects:
+//#define MOCKITO_SHORTHAND
+//#import <OCMockitoIOS/OCMockitoIOS.h>
+
+
+@interface Test : SenTestCase
+@end
+
+@implementation Test
+{
+    // test fixture ivars go here
+}
+
+- (void)testExample
+{
+ 
+}
+
+@end
